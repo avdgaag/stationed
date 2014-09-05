210f53148b723f5a30ace74aa25fa174c79fc0f7
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2014-09-05 20:37:45 +0200

    Also delegate model_name for PaginatingDecorator

--- lib/stationed/generators/templates/paginating_decorator.rb
+++ lib/stationed/generators/templates/paginating_decorator.rb
@@ -3,5 +3,5 @@
 # {ApplicationDecorator}.
 class PaginatingDecorator < Draper::CollectionDecorator
   delegate :current_page, :total_pages, :limit_value, :total_count,
-           :offset_value, :last_page?
+           :offset_value, :last_page?, :model_name
 end
